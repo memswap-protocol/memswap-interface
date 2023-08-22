d0f1703e199ffe2a4ceb271c6063a27d17f67b09
Ted Palmer <etpalmer17@gmail.com>
2023-08-21 21:36:45 -0700

    Fix build error

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,4 @@
 export { default as useTokenList } from './useTokenList'
 export { default as useQuote } from './useQuote'
 export { default as useMounted } from './useMounted'
-export { default as useDeepLinkParams } from './useDeeplinkParams'
+export { default as useDeepLinkParams } from './useDeepLinkParams'
